3491991ebd70224924809b4ea1717660a1719960
nicklausw <nick@berlin.com>
2016-01-15 08:58:07 -0500

    new stuffs

--- snesasm.s
+++ snesasm.s
@@ -1,7 +1,24 @@
 ; this is the file that snesasm should
 ; eventually be able to parse.
 
-; nothing so far tho.
+; will uncomment as things begin to work.
 
-clc
-xce
+; let's set up the rom.
+;.lorom
+;.banksize $8000
+;.rombanks 4
+
+; header
+;.compcheck ; checksum
+;.autoromsize ; compute rom size in header
+;.romsize 7 ; ...or on your own.
+;.name "THIS IS A TEST"
+;.carttype 0 ; rom only
+;.licenseecode 0
+;.version $01
+
+; now the code
+;.bank 0
+
+;clc
+;xce
